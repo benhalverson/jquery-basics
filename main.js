042bf28711dba89d2e18db165f2e4fedd0b07258
Ben Halverson <benhalverson33@gmail.com>
2015-09-14 14:15:56 -0700

    update

--- main.js
+++ main.js
@@ -6,12 +6,32 @@ function init(){
   $('#number-parent').on('click', '.cube', getSquareRoot);
   $('#number-parent').on('dblclick', '.cube', removeCube);
   $('#get-quote').click(getQuote);
+  $('#random-color').click(randomColor);
+}
+
+function randomColor() {
+  var red = Math.floor(Math.random() * 256);
+  var grn = Math.floor(Math.random() * 256);
+  var blu = Math.floor(Math.random() * 256);
+  var alp = Math.random();
+  var rgba = 'rgba('+ red +', '+ grn +', '+ blu +', '+ alp +')';
+  var $color = $('<div>');
+  $color.css('background-color', rgba);
+  $color.addClass('color');
+  $('#color-parent').prepend($color);
 }
 
 function getQuote(){
   var symbol = $('#symbol').val().toUpperCase();
   $.getJSON('http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + symbol + '&callback=?', function(response){
-    console.log(response.LastPrice);
+    console.log('Current Price ' + response.LastPrice);
+
+    var $div = $('<div>');
+    var $high = $('<div>');
+    $div.addClass('quote');
+    $div.text('$' + response.LastPrice);
+    $('#quotes').append($div);
+    console.log('High Price ' + response.High);
   });
 }
 
