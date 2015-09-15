b65d4d7cbe0efdb943870d84581f6ad9295faf29
Ben Halverson <benhalverson33@gmail.com>
2015-09-15 05:05:13 -0700

    get and posting to database work

--- ajax/app.js
+++ ajax/app.js
@@ -1,14 +1,46 @@
 $(function (){
 
   var $orders = $('#orders');
+  var $name = $('#name');
+  var $drink = $('#drink');
+
+  function addOrder(order){
+    $orders.append('<li>name: ' + order.name + ', drink: '+ order.drink +' </li>');
+  }
   $.ajax({
     type: 'GET',
     url: 'http://rest.learncode.academy/api/learncode/friends',
     success: function(orders) {
-      console.log(orders);
+
       $.each(orders, function(index, order){
-        $orders.append('<li>name: ' + order.name + ', drink: '+ order.drink +' </li>');
-      })
+        addOrder(order);
+      });
+    },
+    error: function(){
+      console.error('Error loading orders');
     }
-  })
+  });
+
+  $('#add-order').on('click', function(){
+    var order = {
+      name: $name.val(),
+      drink: $drink.val()
+    };
+    $.ajax({
+      type: 'POST',
+      url: 'http://rest.learncode.academy/api/learncode/friends',
+      data: order,
+      // this way also works
+      // data: {
+      //   name: $name.val(),
+      //   drink: $drink.val()
+      // },
+      success: function(newOrder) {
+        addOrder(newOrder);
+      },
+      error: function(){
+        console.error('error saving order');
+      }
+    })
+  });
 });
